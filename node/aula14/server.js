d2e305dad7dc9c6de5807ac6ff3e313b481440bf
frontEnd-Jair <jair_correa@outlook.com.br>
2025-02-15 17:14:28 -0300

    Setting mongo database access

--- node/aula14/server.js
+++ node/aula14/server.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+//chamar mongoose
+const mongoose = require("mongoose");
+const connectionString =
+  "mongodb+srv://jair-correa:08do09de1991CORREA@cluster0.sltmw.mongodb.net/DATABASE?retryWrites=true&w=majority&appName=Cluster0";
+//fazer conexao do mongoose
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log('Conectei a base de dados');
+    
+    app.emit("OK");//Cria sinal e esctuta para seguir com a conexao
+  }); //notifica sobre a conexao da base de dados
+
 const routes = require("./routes");
 const path = require("path");
 //acesso por desetruturação
@@ -15,7 +28,9 @@ app.set("view engine", "ejs");
 app.use(middlewareGlobal);
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000 --> http://localhost:3000");
-  console.log("Servidor executando na porta 3000");
+app.on("OK", () => {
+  app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000 --> http://localhost:3000");
+    console.log("Servidor executando na porta 3000");
+  });
 });
